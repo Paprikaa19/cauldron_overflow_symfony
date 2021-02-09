b3b9ccd7f71ffdbd0210b4abdc94ba8da0d35576
Alvaro Ariza <alvaroacaliz@gmail.com>
2021-02-09 18:06:42 +0100

    Added some templates with twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
 
     /**
@@ -23,8 +24,15 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Hello: "%s"!', ucwords( str_replace('-', ' ',$slug))
-        ));
+        $answers = [
+            'Im first answer from the DB, dont hit me, im scared',
+            'Hello, Im the second answer for this test',
+            'Are you sure that this is the test that you want to do?'
+        ];
+
+        return $this->render('questions/show.html.twig', [
+            'question' => ucwords( str_replace('-', ' ',$slug)),
+            'answers' => $answers,
+        ]);
     }
 }
